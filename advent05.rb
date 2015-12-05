26a5eae3e9045d1722af470beda8a54f80d9358e
Cara Heacock <caraheacock@gmail.com>
2015-12-05 00:31:52 -0600

    Refactored solutions for day 5 into single regular expressions

--- advent05.rb
+++ advent05.rb
@@ -11,19 +11,33 @@ nice = 0
 nice2 = 0
 
 list.each do |word|
-  # If the word
+  # Part 1 requirements
   # - Contains at least 3 vowels
   # - Contains 2 of the same character in a row
   # - Does NOT contain "ab", "cd", "pq", or "xy"
-  if word.scan(/[aeiou]/).count >= 3 && word[/(.)\1/] && !word[/ab|cd|pq|xy/]
+  
+  # Original solution with 3 regexes
+  # if word.scan(/[aeiou]/).count >= 3 && word[/(.)\1/] && !word[/ab|cd|pq|xy/]
+  #   nice += 1
+  # end
+  
+  # New solution condensed to 1 regex
+  if word[/^(?=.*[aeiou].*[aeiou].*[aeiou])(?=.*(.)\1)(?!.*(?:ab|cd|pq|xy)).*$/]
     nice += 1
   end
   
-  # If the word
+  # Part 2 requirements
   # - Has 2 of the same 2 characters together anywhere in the word
   # - Has 1 character that repeats with 1 character in between
-  if word[/(..).*\1/] && word[/(.).\1/]
-    nice2 += 1
+  
+  # Original solution with 2 regexes
+  # if word[/(..).*\1/] && word[/(.).\1/]
+  #   nice2 += 1
+  # end
+  
+  # New solution condensed to 1 regex
+  if word[/^(?=.*(..).*\1)(?=.*(.).\2).*$/]
+    nice2 +=1
   end
 end
 
